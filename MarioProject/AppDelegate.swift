ba38ad3da63fb51e2f62aef6f595167b24cf8111
SoRush <Sorush@MacBooks-MacBook-Pro.local>
2017-04-04 16:01:03 +0430

    Initial Commit

---
+++ MarioProject/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MarioProject
+//
+//  Created by SoRush on 1/15/1396 AP.
+//  Copyright © 1396 SoRush. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ MarioProject/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MarioProject
+//
+//  Created by SoRush on 1/15/1396 AP.
+//  Copyright © 1396 SoRush. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
